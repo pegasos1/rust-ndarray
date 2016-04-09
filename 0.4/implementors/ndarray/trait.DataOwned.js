34458fea7cd773fbd3e5f2c47f4abf34999c80e1
bluss <bluss@users.noreply.github.com>
2016-04-09 21:53:49 +0200

    Add 0.4 directory

---
+++ 0.4/implementors/ndarray/trait.DataOwned.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['ndarray'] = [];implementors['ndarray'] = [];implementors['ndarray'] = [];implementors['ndarray'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
